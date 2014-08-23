58d988d73dec00982a4ab4f76bf248072b411225
Alex <alexanderwcarnahan@gmail.com>
2014-08-23 12:06:20 -0600

    added more approx methods to Calculus.py and fixed style issues in both files

--- Calculus.py
+++ Calculus.py
@@ -1,20 +1,23 @@
 #Calculus Functions
 
 #example functions
-# F(x) = x^2
+
 def parabola(x):
+    """F(x) = x^2"""
     return x*x
-# F(x) = 2x+1
+
 def line(x):
+    """F(x) = 2x+1"""
     return 2*x
-# F(x) = x^4-3x^3+x-10
+
 def poly(x):
+    """F(x) = x^4-3x^3+x-10"""
     return x*x*x*x-3*x*x*x+x-10
 
-#Integration between (a,b), where F is some function
-#n = number of trapezoids
-#Trapezoidal Approx
-def ATrap(a,b,F,n=10000):
+def trap(a,b,F,n=10000):
+    """Integration between (a,b), where F is some function
+    n = number of trapezoids
+    Trapezoidal Approx"""
     Area = 0
     w = (b-a)/n
     for num in range(1,n+1):
@@ -22,3 +25,37 @@ def ATrap(a,b,F,n=10000):
         h2 = F(a+ w*num)
         Area += h1*w-1/2*(h2-h1)*w
     return Area
+
+def mid(a,b,F,n=10000):
+    """Integration between (a,b), where F is some function
+    n = number of rectangles
+    Midpoint Approx"""
+    Area = 0
+    w = (b-a)/n
+    for i in range(1,n+1):
+        h = F(a+w*(i+1/2))
+        Area += h*w
+    return Area
+
+def right(a,b,F,n=10000):
+    """Integration between (a,b), where F is some function
+    n = number of rectangles
+    Right Approx"""
+    Area = 0
+    w = (b-a)/n
+    for i in range(0,n):
+        h = F(a+w*i)
+        Area += h*w
+    return Area
+
+def left(a,b,F,n=10000):
+    """Integration between (a,b), where F is some function
+    n = number of rectangles
+    Left Approx"""
+    Area = 0
+    w = (b-a)/n
+    for i in range(1,n+1):
+        h = F(a+w*i)
+        Area += h*w
+    return Area
+    
--- Recursives.py
+++ Recursives.py
@@ -1,43 +1,54 @@
 #various recursive algorithms
 
-#quick exponentiation of integers
-def AExp(n,p):
-    if p==0:
-        return 1
-    elif p%2==1:
-        return n*AExp(n,p-1)
-    else:
-        t = AExp(n,p/2)
-        return t*t
+def exp(n,p):
+    """quick exponentiation of integers"""
+    try:
+        if p==0:
+            return 1
+        elif p%2==1:
+            return n*exp(n,p-1)
+        else:
+            t = exp(n,p/2)
+            return t*t
+    except:
+        return "could not calculate (nested too deep)"
     
-#quick get greatest common denominator
-def AGCD(a,b):
-    if -.0001<a-b<.0001:
-        return a
-    elif a<b:
-        return AGCD(b,a)
-    elif a>b:
-        print(a-b)
-        return AGCD(b,a-b)
-    else:
-        return "no GCD"
+def gcd(a,b):
+    """quick get greatest common denominator"""
+    try:
+        if -.0001<a-b<.0001:
+            return a
+        elif a<b:
+            return gcd(b,a)
+        elif a>b:
+            return gcd(b,a-b)
+        else:
+            return "no GCD"
+    except:
+        return "could not find GCD (nested too deep)"
 
-#get nth Fibbonaci number
-def AFib(n):
-    if n <= 0:
-        return 0
-    if n <= 1:
-        return 1
-    else:
-        return AFib(n-1)+AFib(n-2)
-
-#Ackermann's Function
-#total computable function that is not primitive recursive.
-#sometimes used to benchmark compilers for deep recursion 
-def AAckermann(a,b):
-    if a == 0:
-        return b+1
-    elif a != 0 and b == 0:
-        return AAckermann(a-1,1)
-    else:
-        return AAckermann(a-1, AAckermann(a,b-1))
+def fib(n):
+    """get nth Fibbonaci number"""
+    try:
+        if n <= 0:
+            return 0
+        if n <= 1:
+            return 1
+        else:
+            return fib(n-1)+fib(n-2)
+    except:
+        return "could not find "+n+"th fibbonaci number (nested too deep)"
+    
+def ackermann(a,b):
+    """Ackermann's Function
+    total computable function that is not primitive recursive.
+    sometimes used to benchmark compilers for deep recursion"""
+    try:
+        if a == 0:
+            return b+1
+        elif a != 0 and b == 0:
+            return ackermann(a-1,1)
+        else:
+            return ackermann(a-1, ackermann(a,b-1))
+    except:
+        return "nested too deep"
