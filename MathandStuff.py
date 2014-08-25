f279ac80efd8bcb550422bdffe15449a32abfd84
Alex <alexanderwcarnahan@gmail.com>
2014-08-24 20:03:26 -0600

    added some cool methods to practice summations, also came up with an approx for a sqr root

--- MathandStuff.py
+++ MathandStuff.py
@@ -1,6 +1,12 @@
+import random
 #Calculus Functions
 
 #example functions
+
+l1 = [1,1,1,1,1,2]
+l2 = [0]*1000
+for i in range(1000):
+    l2[i]=random.randrange(0,100)
 
 def parabola(x):
     """F(x) = x^2"""
@@ -58,4 +64,48 @@ def left(a,b,F,n=10000):
         h = F(a+w*i)
         Area += h*w
     return Area
-    
+
+def mean(A):
+    """calculates the mean of list A"""
+    return sum(A)/len(A)
+
+def variance(A):
+    """calculates the variance of A"""
+    m = mean(A)
+    v = 0
+    for x in A:
+        v += ((x-m)*(x-m))
+    return v/(len(A)-1)
+
+def stddev(A):
+    """calcs standard deviation"""
+    v = variance(A)
+    return sqroot(v)
+
+def sqroot(num):
+    """method for approx a square root
+    making it up as I go
+    max of 10^10 with precision to 10^-10"""
+    temp = 0
+    for i in range(20):
+        temp += getdigit(i,num, temp)
+    return temp
+
+def getdigit(place,num,temp):
+    """helper to sqroot"""
+    power = 10-place
+    for index in range(1,11):
+        if pow(temp+ index*pow(10, power),2) > num:
+            return (index-1)*pow(10,power)  
+
+def square(num):
+    """calculating a decimal squared using int
+    (exact up to 2 decimal places)  O(num*p)
+    honestly not very good"""
+    n = int(num*100+.1)
+    print(n)
+    s = 0
+    for i in range(n):
+        s += 1+2*i
+    return s/10000
+
